256cd1e92c5cd3b310a98876a06a73dabfa9f842
Hoang Long <thanhlong.vn09@gmail.com>
2017-03-02 21:58:00 +0700

    update full icon kaiz

--- HTML/build/Jscripts/custom.js
+++ HTML/build/Jscripts/custom.js
@@ -1,3 +1,13 @@
 /* ====================================
    Onload functions
    ==================================== */
+$(".site-header .diagonal .btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#product1").offset().top
+    }, 500);
+});
+$(".site-footer .diagonal .btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#product1").offset().top
+    }, 1500);
+});
